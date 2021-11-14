e75ba2fe6db15d1db4af56ee5fabb798a8535844
rosinfotech <rosinfo.tech@gmail.com>
2021-11-14 16:20:29 +0300

    0.0.12

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ module.exports = {
     "rules": {
         "react-hooks/exhaustive-deps": 1,
         "react-hooks/rules-of-hooks": 2,
-        "react/jsx-tag-spacing": [
+        "react/jsx-closing-bracket-location": [
             1,
-            {
-                "closingSlash": "never",
-                "beforeSelfClosing": "always",
-                "afterOpening": "never",
-                "beforeClosing": "allow"
-            }
+            "tag-aligned"
         ],
+        "react/jsx-closing-tag-location": 1,
         "react/jsx-curly-newline": [
             1,
             {
@@ -18,24 +14,9 @@ module.exports = {
                 "singleline": "consistent"
             }
         ],
-        "react/jsx-wrap-multilines": [
+        "react/jsx-curly-spacing": [
             1,
-            {
-                "declaration": "parens-new-line",
-                "assignment": "parens-new-line",
-                "return": "parens-new-line",
-                "arrow": "parens-new-line",
-                "condition": "parens-new-line",
-                "logical": "parens-new-line",
-                "prop": "parens-new-line"
-            }
-        ],
-        "react/jsx-max-props-per-line": [
-            1,
-            {
-                "maximum": 1,
-                "when": "multiline"
-            }
+            "always"
         ],
         "react/jsx-first-prop-new-line": [
             1,
@@ -45,29 +26,51 @@ module.exports = {
             1,
             "first"
         ],
+        "react/jsx-max-props-per-line": [
+            1,
+            {
+                "maximum": 1,
+                "when": "multiline"
+            }
+        ],
         "react/jsx-sort-default-props": 1,
         "react/jsx-sort-props": [
             1,
             {
                 "callbacksLast": true,
-                "shorthandLast": true,
-                "shorthandFirst": false,
                 "ignoreCase": false,
                 "noSortAlphabetically": false,
-                "reservedFirst": true
+                "reservedFirst": true,
+                "shorthandFirst": false,
+                "shorthandLast": true
             }
         ],
-        "react/jsx-closing-bracket-location": [
-            1,
-            "tag-aligned"
-        ],
-        "react/jsx-closing-tag-location": 1,
-        "react/jsx-curly-spacing": [
+        "react/jsx-tag-spacing": [
             1,
             {
-                "when": "always",
-                "children": true
+                "afterOpening": "never",
+                "beforeClosing": "allow",
+                "beforeSelfClosing": "always",
+                "closingSlash": "never"
+            }
+        ],
+        "react/jsx-wrap-multilines": [
+            1,
+            {
+                "arrow": "parens-new-line",
+                "assignment": "parens-new-line",
+                "condition": "parens-new-line",
+                "declaration": "parens-new-line",
+                "logical": "parens-new-line",
+                "prop": "parens-new-line",
+                "return": "parens-new-line"
+            }
+        ],
+        "react/prop-types": [
+            1,
+            {
+                "skipUndeclared": true
             }
         ]
-    }    
-}
+    }
+};
